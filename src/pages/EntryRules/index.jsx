e0c1d93ced5925dbfed084793d3610ad086c47fc
iqbal <1257148iqbal@gmail.com>
2022-09-06 16:05:34 +0600

    modify status table

--- src/pages/EntryRules/index.jsx
+++ src/pages/EntryRules/index.jsx
@@ -13,7 +13,7 @@ const EntryRules = () => {
             aria-expanded="true"
             aria-controls="collapseOne"
           >
-            Accordion Item #1
+            ORGANIZER
           </button>
         </h2>
         <div
@@ -22,14 +22,27 @@ const EntryRules = () => {
           aria-labelledby="headingOne"
         >
           <div className="accordion-body">
-            <strong>This is the first item's accordion body.</strong> It is
-            shown by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
+            <div className="pb-2">
+              <b>The Photographic Society of Singapore, Singapore (PSS)</b>{" "}
+              <br />
+              Chairman: Jack Goh, EFIAP, PPSA, GPU CR2, GPU APHRODITE, APSS,
+              APSNJ <br />
+              E-mail: demo.org
+            </div>
+            <div className="pb-2">
+              <b>Greek Artistic Photography, Greece (GAP)</b> <br />
+              Chairman: Manolis Metzakis, EFIAP/p, ESFIAP, GPU-ZEUS, GPU-CR5,
+              QPSA, APSA
+              <br />
+              E-mail: demo.org
+            </div>
+            <div className="pb-2">
+              <b>Atlantic Institute of Photography, USA (AIP)</b> <br />
+              hairman: Kah-Wai Lin, EPSA, SPSA, AFIAP, GPU-CR2, GPU-VIP2,
+              GPU-APHRODITE
+              <br />
+              E-mail: demo.org
+            </div>
           </div>
         </div>
       </div>
@@ -43,7 +56,7 @@ const EntryRules = () => {
             aria-expanded="true"
             aria-controls="collapseTwo"
           >
-            Accordion Item #2
+            PARTICIPATION
           </button>
         </h2>
         <div
@@ -52,14 +65,72 @@ const EntryRules = () => {
           aria-labelledby="headingTwo"
         >
           <div className="accordion-body">
-            <strong>This is the second item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML can go within the <code>.accordion-body</code>,
-            though the transition does limit overflow.
+            <strong>IMAGE AND ENTRY REQUIREMENTS </strong> This exhibition is
+            open to anyone; however, an entry may be rejected when the Sponsor
+            or the Exhibition Organizers, in their reasonable discretion,
+            believes the entry does not conform to exhibition rules and these
+            Conditions of Entry. Membership in any photographic organization is
+            not required. <br/> <br/>
+            Sanctions: Entries will not be accepted from any
+            entrant who is currently sanctioned by PSA. <br/> <br/>
+            PSA Star Ratings To <br/>
+
+            receive proper Star ratings credit from PSA, entrants must provide
+            their names and country exactly the same in each exhibition. Aliases
+            are not permitted. Please contact PSA in the event of name changes
+            or relocating to another country. Using one’s name differently in
+            different exhibition exposes the entrant to the risk that many of
+            their acceptances may not be recognized by PSA Star Ratings <br/> <br/>
+            
+            Image Creation <br/> Entries must originate as photographs (image-captures of
+            objects via light sensitivity) made by the entrant on photographic
+            emulsion or acquired digitally. <br/>  <br/> 
+            Certification: <br/>
+            By virtue of
+            submitting an image, the entrant certifies the work as his or her
+            own (.Images may not incorporate elements produced by anyone else
+            (for example: clip art, images or art by others downloaded from the
+            Internet). Aliases are not allowed. The entrant permits the sponsors
+            to reproduce all or part of the entered material free of charge for
+            publication and/or display in media related to the exhibition. This
+            may include low resolution posting on a website. Note: Entrants who
+            indicate that their images may not be reproduced or used “will not
+            be eligible for awards” or inclusion in audio-visuals of the
+            exhibition “and could be subject to disqualification” by the
+            exhibition sponsors. The exhibition assumes no liability of any
+            misuse of copyright <br/><br/> 
+            
+            Alteration and Computer Generation Subject to
+            Divisional restrictions (particularly Nature, Photo Travel, and
+            Photojournalism) images may be altered, either electronically or
+            otherwise, by the maker. Adjustments to enhance images or creatively
+            modify images are allowed providing the underlying photograph is
+            retained in a way that is obvious to the viewer. Images may not be
+            constructed entirely with a computer, and must be the sole work of
+            the author <br/><br/> 
+            
+            Re-use of accepted images: Once an image has been
+            accepted in this exhibition it may not be entered again in any
+            future instances of this exhibition. It may, of course, be entered
+            in any other PSA/FIAP recognised exhibitions but must always have
+            the same title. <br/><br/> 
+            
+            Entry: An Entry consists of, up to and including,
+            four (4) images entered by a single entrant into the same Section.
+            An entrant may only enter a specific Section once. <br/><br/> 
+            
+            Titles: <br/> 
+            Each image must have a unique title that is a description of the image.
+            That unique title must be used for entry of that image or of an
+            identical Image into any and all PSA-Recognized exhibitions. Titles
+            must be 35 characters or fewer. No titles may be visible to the
+            judges, and nothing in the image may identify the maker. Titles may
+            not include file extensions such as .jpg or .jpeg (or any other
+            camera capture filenames such as IMG 471). Titles may not consist of
+            personal identifiers possibly augmented by a number; or include
+            words such as “untitled” or “no title”. Titles may not consist
+            solely of numbers unless those numbers are prominently contained
+            within the image, such as a contestant number in a race. 
           </div>
         </div>
       </div>
@@ -73,7 +144,7 @@ const EntryRules = () => {
             aria-expanded="true"
             aria-controls="collapseThree"
           >
-            Accordion Item #3
+            SECTIONS
           </button>
         </h2>
         <div
@@ -82,14 +153,14 @@ const EntryRules = () => {
           aria-labelledby="headingThree"
         >
           <div className="accordion-body">
-            <strong>This is the fourth item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML,
-            though the transition does limit overflow.
+            <ul>
+              <li> 4 Sections:</li>
+              <li> Open Color (color only) PIDC</li>
+              <li> Open Monochrome (monochrome only) PIDM</li>
+              <li> Nature (color & monochrome) ND</li>
+              <li> Photo Travel (color & monochrome) PTD</li>
+              <b>Each participant can enter up to 4 images per section.</b>
+            </ul>
           </div>
         </div>
       </div>
@@ -104,7 +175,7 @@ const EntryRules = () => {
             aria-expanded="true"
             aria-controls="collapseFour"
           >
-            Accordion Item #4
+            VENTS CALENDAR
           </button>
         </h2>
         <div
@@ -113,14 +184,14 @@ const EntryRules = () => {
           aria-labelledby="headingFour"
         >
           <div className="accordion-body">
-            <strong>This is the third item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML,
-            though the transition does limit overflow.
+            <ul>
+              <li>Closing Date: 26 June 2023</li>
+              <li>Judging Date: 2 – 3 July 2023</li>
+              <li>Notification Date: 17 July 2023</li>
+              <li>Catalog Upload Date: 21 August 2023</li>
+              <li>e-Exhibition Date: 23 August – 23 November 2023</li>
+              <li>Award Mailing Date: 31 August 2023</li>
+            </ul>
           </div>
         </div>
       </div>
@@ -135,7 +206,7 @@ const EntryRules = () => {
             aria-expanded="true"
             aria-controls="collapseFive"
           >
-            Accordion Item #5
+           ENTRY FEE
           </button>
         </h2>
         <div
@@ -144,14 +215,13 @@ const EntryRules = () => {
           aria-labelledby="headingFive"
         >
           <div className="accordion-body">
-            <strong>This is the fifth item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML,
-            though the transition does limit overflow.
+              <ul>
+                <li>Online entry only at http://www.pacificatlantic-photo.com/</li>
+                <li>1 section: US$30.00; 2 sections: US$35.00; 3 sections: US$42.00; 4 sections: US$48.00</li>
+                <li>Payment by PayPal at exhibition website or PayPal to kahwailin@outlook.com</li>
+                <li>Entries receive without fee will not be judged.</li>
+                <li>Group entry with than 6 participants will receive 10% discount.</li>
+              </ul>
           </div>
         </div>
       </div>
@@ -164,9 +234,9 @@ const EntryRules = () => {
             data-bs-toggle="collapse"
             data-bs-target="#collapseSix"
             aria-expanded="true"
-            aria-controls="collapseSix"            
+            aria-controls="collapseSix"
           >
-            Accordion Item #6
+            SUBMISSION OF WORKS
           </button>
         </h2>
         <div
@@ -175,15 +245,135 @@ const EntryRules = () => {
           aria-labelledby="headingSix"
         >
           <div className="accordion-body">
-            <strong>This is the sixth item's accordion body.</strong> It is
-            hidden by default, until the collapse plugin adds the appropriate
-            classNamees that we use to style each element. These classNamees
-            control the overall appearance, as well as the showing and hiding
-            via CSS transitions. You can modify any of this with custom CSS or
-            overriding our default variables. It's also worth noting that just
-            about any HTML,
-            though the transition does limit overflow.
-            
+          Digital image format: JPEG, sRGB color profile, maximum image width (horizontal) is 1920 pixels and maximum image height (vertical) is 1080 pixels, maximum file size is 2 MB or less.
+          <br/>
+          <b>All images will be displayed on the Sony Bravia Full HDTV for judging in the public.</b>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingSeven">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseSeven"
+            aria-expanded="true"
+            aria-controls="collapseSeven"
+          >
+            JURY
+          </button>
+        </h2>
+        <div
+          id="collapseSeven"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingSeven"
+        >
+          <div className="accordion-body">
+          Digital image format: JPEG, sRGB color profile, maximum image width (horizontal) is 1920 pixels and maximum image height (vertical) is 1080 pixels, maximum file size is 2 MB or less.
+          <br/>
+          <b>All images will be displayed on the Sony Bravia Full HDTV for judging in the public.</b>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingEight">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseEight"
+            aria-expanded="true"
+            aria-controls="collapseEight"
+          >
+            AWARDS
+          </button>
+        </h2>
+        <div
+          id="collapseEight"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingEight"
+        >
+          <div className="accordion-body">
+          Digital image format: JPEG, sRGB color profile, maximum image width (horizontal) is 1920 pixels and maximum image height (vertical) is 1080 pixels, maximum file size is 2 MB or less.
+          <br/>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingNine">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseNine"
+            aria-expanded="true"
+            aria-controls="collapseNine"
+          >
+            DEFINITIONS
+          </button>
+        </h2>
+        <div
+          id="collapseNine"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingNine"
+        >
+          <div className="accordion-body">
+          Digital image format: JPEG, sRGB color profile, maximum image width (horizontal) is 1920 pixels and maximum image height (vertical) is 1080 pixels, maximum file size is 2 MB or less.
+          <br/>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingTen">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTen"
+            aria-expanded="true"
+            aria-controls="collapseTen"
+          >
+            NOTIFICATION & CATALOG
+          </button>
+        </h2>
+        <div
+          id="collapseTen"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingTen"
+        >
+          <div className="accordion-body">
+          Digital image format: JPEG, sRGB color profile, maximum image width (horizontal) is 1920 pixels and maximum image height (vertical) is 1080 pixels, maximum file size is 2 MB or less.
+          <br/>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingEleven">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseEleven"
+            aria-expanded="true"
+            aria-controls="collapseEleven"
+          >
+            CONTACTS
+          </button>
+        </h2>
+        <div
+          id="collapseEleven"
+          className="accordion-collapse collapse show"
+          aria-labelledby="headingEleven"
+        >
+          <div className="accordion-body">
+          Digital image format: JPEG, sRGB color profile, maximum image width (horizontal) is 1920 pixels and maximum image height (vertical) is 1080 pixels, maximum file size is 2 MB or less.
+          <br/>
           </div>
         </div>
       </div>
